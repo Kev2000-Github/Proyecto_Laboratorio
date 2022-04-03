9c6992b82b4fe91c1dd3bf68bc691e1aca3f4abd
Oswaldo Yanez <oswaldo.business31@gmail.com>
2022-04-03 19:08:51 -0400

    agregando personadao.

--- src/DAO/PersonaDao.java
+++ src/DAO/PersonaDao.java
@@ -1,47 +1,49 @@
 package DAO;
 
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Vector;
 
-import modelos.Usuario;
+import modelos.Persona;
 
-//MOCK IMPLEMENTATION, se necesita conexion a bd para obtener la data real
-public class PersonaDao implements IDao<Usuario> {
-    
-    private Vector<Usuario> usuarios = new Vector();
-    
-    public PersonaDao() {
-        usuarios.add(new Usuario("kevin","admin"));
-        usuarios.add(new Usuario("oswald","root"));
-    }
-    
+public class PersonaDao implements IDao<Persona> {
+
+    private Vector<Persona> personas = new Vector();
+
     @Override
-    public Usuario get(long id) {
-        return usuarios.get((int) id);
+    public Persona get(long id) {
+        return personas.get((int) id);
     }
-    
+
     @Override
-    public Vector<Usuario> getAll() {
-        return usuarios;
+    public Vector<Persona> getAll() {
+        return personas;
     }
-    
+
     @Override
-    public void save(Usuario user) {
-        usuarios.add(user);
+    public void save(Persona user) {
+        personas.add(user);
     }
-    
+
     @Override
-    public void update(Usuario user, String[] params) {
-        user.setUsername(Objects.requireNonNull(
-          params[0], "Name cannot be null"));
-        user.setPassword(Objects.requireNonNull(
-          params[1], "Email cannot be null"));
-        
-        usuarios.add(user);
+    public void update(Persona person, String[] params) {
+        person.setCedula(Objects.requireNonNull(
+                params[0], "Cedula cannot be null"));
+        person.setNombre(Objects.requireNonNull(
+                params[1], "Nombre cannot be null"));
+        person.setApellido(Objects.requireNonNull(
+                params[2], "Apellido cannot be null"));
+        person.setDireccion(Objects.requireNonNull(
+                params[3], "Direccion cannot be null"));
+        person.setTelefono(Objects.requireNonNull(
+                params[4], "Direccion cannot be null"));
+
+        personas.add(person);
     }
-    
+
     @Override
-    public void delete(Usuario user) {
-        usuarios.remove(user);
+    public void delete(Persona person) {
+        personas.remove(person);
     }
+
 }
