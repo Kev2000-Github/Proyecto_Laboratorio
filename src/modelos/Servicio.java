bcac69153651675693002877eec2337aeef59b1f
Oswaldo Yanez <oswaldo.business31@gmail.com>
2022-04-11 19:46:01 -0400

    feat: Modelo servicio

--- src/modelos/Servicio.java
+++ src/modelos/Servicio.java
@@ -2,6 +2,41 @@ package modelos;
 
 public class Servicio {
 
-	public Servicio(){}
+	private String codigo;
+	private String nombre;
+	private String tipo;
+
+	public Servicio() {
+	};
+
+	public Servicio(String codigo, String nombre, String tipo) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.tipo = tipo;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 
 }
