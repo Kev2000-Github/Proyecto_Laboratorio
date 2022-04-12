8ac69ecce6f85efedf7e0299c1e8ba015a66e2c3
Oswaldo Yanez <oswaldo.business31@gmail.com>
2022-04-12 15:24:18 -0400

    feat : Modelo fundacion y CRUD FundacionDao.

--- src/modelos/Fundacion.java
+++ src/modelos/Fundacion.java
@@ -2,4 +2,51 @@ package modelos;
 
 public class Fundacion {
 
+    private String id;
+    private String nombre;
+    private double presupuesto;
+    private double porcentaje_partido_anual;
+
+    public Fundacion() {
+    };
+
+    public Fundacion(String id, String nombre, double presupuesto, double porcentaje_partido_anual) {
+        this.id = id;
+        this.nombre = nombre;
+        this.presupuesto = presupuesto;
+        this.porcentaje_partido_anual = porcentaje_partido_anual;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(double presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public double getPorcentaje_partido_anual() {
+        return Porcentaje_partido_anual;
+    }
+
+    public void setPorcentaje_partido_anual(double porcentaje_partido_anual) {
+        this.Porcentaje_partido_anual = Porcentaje_partido_anual;
+    }
+
 }
